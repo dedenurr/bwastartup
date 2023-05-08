c4d62781e21ef118767c37f54a1a442458685a81
dedenurr <dedenurrahman.git@gmail.com>
2023-05-08 08:52:36 +0700

    Login endpoint

--- main.go
+++ main.go
@@ -21,17 +21,15 @@ import (
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 	userHandler := handler.NewUserHandler(userService)
-	
+
+
 	router := gin.Default()
 
 	api := router.Group("/api/v1")
 	api.POST("/users",userHandler.RegisterUser)
+	api.POST("/sessions",userHandler.Login)
 	router.Run()
 	
-	// input dari user
-	// handler, maping input dari user -> struct input
-	// service : melakukan maping dari struct input ke struct User
-	// repository
-	// db
+
 } 
 
