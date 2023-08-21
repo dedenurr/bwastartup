39758a58f2e76fc437698650d81189fcfd9ba15e
dede nurrahman <dedenurrahman.git@gmail.com>
2023-08-21 20:54:18 +0700

    get campaigns endpoint formstter

--- handler/campaign.go
+++ handler/campaign.go
@@ -29,7 +29,7 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 		return
 	}
 
-	response := helper.APIResponse("List Of Campaigns", http.StatusOK, "success", campaigns)
+	response := helper.APIResponse("List Of Campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 	return
 
